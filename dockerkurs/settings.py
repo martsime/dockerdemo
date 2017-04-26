a564f4b167519c776a8e74c3c2aade90cb365f40
martsime <simensen94@gmail.com>
2017-04-26 12:05:33 +0200

    Update static files

--- dockerkurs/settings.py
+++ dockerkurs/settings.py
@@ -130,4 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+if not DEBUG:
+	STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
